2d07b388bdb12b474f095acc3f4624cd025a2efa
Sevde <sevdeorscelik@gmail.com>
2022-06-01 12:13:49 +0200

    created components and little styling

---
+++ src/component/Login.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import { Form, Segment, Button, Grid, Message } from 'semantic-ui-react'
+import styles from './login.module.css'
+
+
+const Login = () => {
+  return (
+    <Grid textAlign="center" verticalAlign="middle" className={styles.container}>
+
+    </Grid>
+  )
+};
+
+export default Login;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,32 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import 'semantic-ui-css/semantic.min.css'
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
+import SignUp from './component/SignUp'
+import Login from './component/Login'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+
+
+
+const Root = () => {
+    return (
+        <Router>
+            <nav className='navbar'>
+                <Link to='/'> App </Link>
+                <Link to='/signup'> Sign Up </Link>
+                <Link to='/login'> Login </Link>
+            </nav>
+            <Routes>
+                <Route path='/' element={<App />} />
+                <Route path='/signup' element={<SignUp />} />
+                <Route path='/login' element={<Login />} />
+            </Routes>
+        </Router>
+    )
+};
+
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(<Root />)
