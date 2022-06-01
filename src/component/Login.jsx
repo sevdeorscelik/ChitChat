eef48c5506aef00a99715e8f349118a861969d5c
Sevde <sevdeorscelik@gmail.com>
2022-06-01 15:26:13 +0200

    created form

--- src/component/Login.jsx
+++ src/component/Login.jsx
@@ -1,14 +1,51 @@
 import React from "react"
-import { Form, Segment, Button, Grid, Message } from 'semantic-ui-react'
-import styles from './login.module.css'
+import { Form, Segment, Button, Grid, Message } from "semantic-ui-react"
+import { Link } from "react-router-dom";
+import styles from "./login.module.css"
+import { useForm } from "react-hook-form";
 
 
 const Login = () => {
-  return (
-    <Grid textAlign="center" verticalAlign="middle" className={styles.container}>
 
-    </Grid>
-  )
+    const handleSubmit = (e) => {
+        e.preventDefault()
+    }
+    return (
+        <Grid textAlign="center" className={styles.container}>
+            <Grid.Column style={{ maxWidth: 500, marginTop: "2rem" }}>
+            <h1 className={styles.formHeader}>ChitChat<span>.io</span> </h1>
+                <Form className={styles.form} onSubmit={handleSubmit} >
+                    <Segment>
+                        <Form.Input
+                            fluid
+                            size="large"
+                            icon="mail"
+                            iconPosition="left"
+                            name="email"
+                            placeholder="Email Adresse"
+                            type="email"
+                        />
+                        <Form.Input
+                            fluid
+                            size="large"
+                            icon="lock"
+                            iconPosition="left"
+                            name="password"
+                            placeholder="********"
+                            type="password"
+                        />
+                        <Button color="purple" fluid size="large">
+                            Login
+                        </Button>
+                    </Segment>
+
+                </Form>
+                <Message>
+                    <span> Don't have an account? </span><Link to="/signup">Sign up</Link>
+                </Message>
+            </Grid.Column>
+        </Grid>
+    )
 };
 
 export default Login;
--- src/component/SignUp.jsx
+++ src/component/SignUp.jsx
@@ -1,11 +1,59 @@
 import React from "react"
+import { Form, Segment, Button, Grid, Message } from "semantic-ui-react"
+import { Link } from "react-router-dom";
+import styles from "./signup.module.css"
+
 
 const SignUp = () => {
-  return (
-    <div>
-      signup
-    </div>
-  )
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+    }
+    return (
+        <Grid textAlign="center" className={styles.container}>
+            <Grid.Column style={{ maxWidth: 500, marginTop: "2 rem" }}>
+                <h1 className={styles.formHeader}>ChitChat<span>.io</span> </h1>
+                <Form className={styles.form} onSubmit={handleSubmit} >
+                    <Segment>
+                    <Form.Input
+                            fluid
+                            size="large"
+                            icon="user"
+                            iconPosition="left"
+                            name="username"
+                            placeholder="Username"
+                            type="text"
+                        />
+                        <Form.Input
+                            fluid
+                            size="large"
+                            icon="mail"
+                            iconPosition="left"
+                            name="email"
+                            placeholder="E-mail Adresse"
+                            type="email"
+                        />
+                        <Form.Input
+                            fluid
+                            size="large"
+                            icon="lock"
+                            iconPosition="left"
+                            name="password"
+                            placeholder="********"
+                            type="password"
+                        />
+                        <Button color="purple" fluid size="large">
+                            Sign up
+                        </Button>
+                    </Segment>
+
+                </Form>
+                <Message>
+                    <span> Do you already have an account? </span><Link to="/login">Login</Link>
+                </Message>
+            </Grid.Column>
+        </Grid>
+    )
 };
 
 export default SignUp;
