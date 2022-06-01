24f69f7c44bd34262f7417a6085c0f35f17308f2
Sevde <sevdeorscelik@gmail.com>
2022-06-01 20:12:08 +0200

    form validation finished

--- src/component/Login.jsx
+++ src/component/Login.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { Form, Segment, Button, Grid, Message } from "semantic-ui-react"
 import { Link } from "react-router-dom";
 import styles from "./login.module.css"
@@ -7,14 +7,22 @@ import { useForm } from "react-hook-form";
 
 const Login = () => {
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
+    const { register, formState: { errors }, handleSubmit, setValue } = useForm()
+
+    useEffect(() => {
+        register("email", { required: true, message: 'custom message'});
+        register("password", { required: true, minLength: 6 });
+        
+    }, [])
+
+    const onSubmit = (data, e) => {
+        console.log(data);
     }
     return (
         <Grid textAlign="center" className={styles.container}>
             <Grid.Column style={{ maxWidth: 500, marginTop: "2rem" }}>
-            <h1 className={styles.formHeader}>ChitChat<span>.io</span> </h1>
-                <Form className={styles.form} onSubmit={handleSubmit} >
+                <h1 className={styles.formHeader}>ChitChat<span>.io</span> </h1>
+                <Form className={styles.form} onSubmit={handleSubmit(onSubmit)} >
                     <Segment>
                         <Form.Input
                             fluid
@@ -24,7 +32,16 @@ const Login = () => {
                             name="email"
                             placeholder="Email Adresse"
                             type="email"
+                            
+                            autoComplete='on'
+                            onChange={(e, { name, value }) => {
+                                setValue(name, value)
+                            }}
+                            error = {errors.email ? true : false}
                         />
+                        
+                        
+                                            
                         <Form.Input
                             fluid
                             size="large"
@@ -33,7 +50,14 @@ const Login = () => {
                             name="password"
                             placeholder="********"
                             type="password"
+                            autoComplete='on'
+                            onChange={(e, { name, value }) => {
+                                setValue(name, value)
+                            }}
+                            error = {errors.password ? true : false}
                         />
+                        
+                        
                         <Button color="purple" fluid size="large">
                             Login
                         </Button>
--- src/component/SignUp.jsx
+++ src/component/SignUp.jsx
@@ -1,21 +1,31 @@
-import React from "react"
+import React, {useEffect} from "react"
 import { Form, Segment, Button, Grid, Message } from "semantic-ui-react"
+import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 import styles from "./signup.module.css"
 
 
 const SignUp = () => {
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
+    const { register, formState: { errors }, handleSubmit, setValue } = useForm()
+
+    useEffect(() => {
+        register("username", { required: true })
+        register("email", { required: true });
+        register("password", { required: true, minLength: 6 });
+    }, [])
+
+    const onSubmit = (data, e) => {
+        console.log(data);
     }
+
     return (
         <Grid textAlign="center" className={styles.container}>
             <Grid.Column style={{ maxWidth: 500, marginTop: "2 rem" }}>
                 <h1 className={styles.formHeader}>ChitChat<span>.io</span> </h1>
-                <Form className={styles.form} onSubmit={handleSubmit} >
+                <Form className={styles.form} onSubmit={handleSubmit(onSubmit)} >
                     <Segment>
-                    <Form.Input
+                        <Form.Input
                             fluid
                             size="large"
                             icon="user"
@@ -23,6 +33,12 @@ const SignUp = () => {
                             name="username"
                             placeholder="Username"
                             type="text"
+                            autoComplete='on'
+                            onChange={(e, { name, value }) => {
+                                setValue(name, value)
+                            }}
+                            error = {errors.username ? true : false}
+                           
                         />
                         <Form.Input
                             fluid
@@ -32,6 +48,11 @@ const SignUp = () => {
                             name="email"
                             placeholder="E-mail Adresse"
                             type="email"
+                            autoComplete='on'
+                            onChange={(e, { name, value }) => {
+                                setValue(name, value)
+                            }}
+                            error = {errors.email ? true : false}
                         />
                         <Form.Input
                             fluid
@@ -41,6 +62,11 @@ const SignUp = () => {
                             name="password"
                             placeholder="********"
                             type="password"
+                            autoComplete='on'
+                            onChange={(e, { name, value }) => {
+                                setValue(name, value)
+                            }}
+                            error = {errors.password ? true : false}
                         />
                         <Button color="purple" fluid size="large">
                             Sign up
