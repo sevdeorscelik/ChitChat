213361ec367fccd036299bf243248f9232d28f0f
Sevde <sevdeorscelik@gmail.com>
2022-06-01 22:42:21 +0200

    firebase fixed

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -1,7 +1,8 @@
-import firebase from 'firebase/app'
-import 'firebase/auth'
-import 'firebase/database'
-import 'firebase/storage'
+import firebase from 'firebase/compat/app'
+import 'firebase/compat/auth'
+import 'firebase/compat/database'
+import 'firebase/compat/storage'
+import { initializeApp } from 'firebase/app';
 
 
 
